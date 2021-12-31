a84ce901829fe260fb28aa65339b1ee8a0105e7b
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-12-31 10:38:45 +0100

    Understanding Arrow Functions

--- refresh/play.js
+++ refresh/play.js
@@ -4,6 +4,38 @@ let hasHobbies = true
 
 age = 30
 
+const summarizeUser = (userName, userAge, userHasHobby) => {
+	return (
+		'Name is ' +
+		userName +
+		', age is ' +
+		userAge +
+		' and the user has hobbies: ' +
+		userHasHobby
+	)
+}
+
+// const add = (a, b) => {
+// 	return a + b
+// }
+
+// const add = (a, b) => a + b
+// const addOne = (a) => a + 1
+const addRandom = () => 1 + 2
+
+/*
+const summarizeUser = function (userName, userAge, userHasHobby) {
+	return (
+		'Name is ' +
+		userName +
+		', age is ' +
+		userAge +
+		' and the user has hobbies: ' +
+		userHasHobby
+	)
+}
+*/
+/*
 function summarizeUser(userName, userAge, userHasHobby) {
 	return (
 		'Name is ' +
@@ -14,5 +46,11 @@ function summarizeUser(userName, userAge, userHasHobby) {
 		userHasHobby
 	)
 }
+
+*/
+
+// console.log(add(1, 2))
+// console.log(addOne(1))
+console.log(addRandom())
 
 console.log(summarizeUser(name, age, hasHobbies))
