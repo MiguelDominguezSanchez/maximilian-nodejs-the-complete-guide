2f630fec75f0592c1e173f36c7c9338498d8333f
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-12-31 17:29:59 +0100

    Async Code & Promises

---
+++ refresh/play_1.js
@@ -0,0 +1,60 @@
+// Working with Objects, Properties & Methods
+
+const person = {
+	name: 'Max',
+	age: 29,
+	greet() {
+		console.log('Hi, I am ' + this.name)
+	},
+}
+
+const printName = ({ name }) => {
+	console.log(name)
+}
+
+printName(person)
+
+const { name, age } = person
+console.log(name, age)
+
+const hobbies = ['Sports', 'Cooking']
+const [hobby1, hobby2] = hobbies
+console.log(hobby1, hobby2)
+
+/*
+// console.log(person)
+
+person.greet()
+
+const copiedPerson = { ...person }
+console.log(copiedPerson)
+
+const hobbies = ['Sports', 'Cooking']
+// for (let hobby of hobbies) {
+// 	console.log(hobby)
+// }
+
+// console.log(
+// 	hobbies.map((hobby) => {
+// 		return 'Hobby:' + hobby
+// 	})
+// )
+
+// console.log(hobbies.map((hobby) => 'Hobby: ' + hobby))
+// console.log(hobbies)
+// hobbies.push('Programming')
+// const copiedArray = hobbies.slice()
+const copiedArray = [...hobbies]
+// console.log(hobbies)
+console.log(copiedArray)
+
+// const toArray = (arg1, arg2, arg3) => {
+// 	return [arg1, arg2, arg3]
+// }
+
+const toArray = (...args) => {
+	return args
+}
+
+console.log(toArray(1, 2, 3, 4))
+*/
