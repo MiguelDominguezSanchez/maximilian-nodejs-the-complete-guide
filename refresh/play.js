3d5d226d9458954e407ec3bb96e201f83337f277
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-12-31 12:54:55 +0100

    Understanding Spread & Rest Operators

--- refresh/play.js
+++ refresh/play.js
@@ -1,6 +1,5 @@
-// Working with Objects, Properrties & Methods
+// Working with Objects, Properties & Methods
 
-/*
 const person = {
 	name: 'Max',
 	age: 29,
@@ -12,7 +11,9 @@ const person = {
 // console.log(person)
 
 person.greet()
-*/
+
+const copiedPerson = { ...person }
+console.log(copiedPerson)
 
 const hobbies = ['Sports', 'Cooking']
 // for (let hobby of hobbies) {
@@ -27,5 +28,18 @@ const hobbies = ['Sports', 'Cooking']
 
 // console.log(hobbies.map((hobby) => 'Hobby: ' + hobby))
 // console.log(hobbies)
-hobbies.push('Programming')
-console.log(hobbies)
+// hobbies.push('Programming')
+// const copiedArray = hobbies.slice()
+const copiedArray = [...hobbies]
+// console.log(hobbies)
+console.log(copiedArray)
+
+// const toArray = (arg1, arg2, arg3) => {
+// 	return [arg1, arg2, arg3]
+// }
+
+const toArray = (...args) => {
+	return args
+}
+
+console.log(toArray(1, 2, 3, 4))
