3f7514285d2e102546bc8aa4d3bd735d6c37f6ff
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-12-31 16:56:37 +0100

    Destructuring

--- refresh/play.js
+++ refresh/play.js
@@ -8,6 +8,20 @@ const person = {
 	},
 }
 
+const printName = ({ name }) => {
+	console.log(name)
+}
+
+printName(person)
+
+const { name, age } = person
+console.log(name, age)
+
+const hobbies = ['Sports', 'Cooking']
+const [hobby1, hobby2] = hobbies
+console.log(hobby1, hobby2)
+
+/*
 // console.log(person)
 
 person.greet()
@@ -43,3 +57,4 @@ const toArray = (...args) => {
 }
 
 console.log(toArray(1, 2, 3, 4))
+*/
