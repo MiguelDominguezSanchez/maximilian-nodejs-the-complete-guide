88db59ad982561836c1829bb1a6f421179df82a8
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-01-09 18:30:09 +0100

    Converting HTML Files to Pug

--- 6/app.js
+++ 6/app.js
@@ -18,8 +18,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-	res.status(404)
-	res.sendFile(path.join(__dirname, 'views', '404.html'))
+	res.status(404).render('404')
 })
 
 app.listen(3000)
